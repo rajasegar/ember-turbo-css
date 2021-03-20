488307a042e33e8507020debf751fa367e8324ac
Rajasegar <rajasegar.c@gmail.com>
2021-03-20 11:39:53 +0530

    chore: first commit

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 'use strict';
 
 module.exports = {
-  name: require('./package').name
+  name: require('./package').name,
+  included: (app) => {
+    this.app = app;
+    app.import('vendor/turbocss/turbocss.browser.watch.min.js');
+  }
 };
